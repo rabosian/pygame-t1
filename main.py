6da533aeb818842e95246c3e45298706c224935e
rabosian <msann1356@gmail.com>
2022-10-12 08:27:08 -0400

    finish before sound effects

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+import os
+pygame.font.init()
 
-# display constant
+# SCREEN
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("space war")
@@ -10,14 +12,118 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
+BORDER = pygame.Rect(WIDTH/2-5, 0, 10, HEIGHT)
+
+HEALTH_FONT = pygame.font.SysFont('comicsans', 30)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+# Frames per second: moves 60 pixels per second
 FPS = 60
 
-def draw_window():
-    WIN.fill(WHITE)
+# BULLET
+BULLET_VEL = 7
+MAX_BULLETS = 3  # max bullets on the screen
+
+# USER EVENT 1,2
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
+
+# SPACESHIP
+VEL = 5  # velocity
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png'))
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_red.png'))
+SPACE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
+
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+
+###############################################################
+def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
+    # .blit(): draw a surface (text OR images)
+    WIN.blit(SPACE, (0,0))
+    pygame.draw.rect(WIN, BLACK, BORDER)
+
+    red_health_text = HEALTH_FONT.render("Health: " + str(red_health), 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), 1, WHITE)
+
+    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
+    WIN.blit(yellow_health_text, (10, 10))
+
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    
+    for b in red_bullets:
+        pygame.draw.rect(WIN, RED, b)
+    for b in yellow_bullets:
+        pygame.draw.rect(WIN, YELLOW, b)
+
     pygame.display.update()  # always update display whenever made changes
 
 
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # UP
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # LEFT
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.width < HEIGHT:  # DOWN
+        yellow.y += VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.height < BORDER.x:  # RIGHT
+        yellow.x += VEL
+
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # UP
+        red.y -= VEL
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  # LEFT
+        red.x -= VEL
+    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.width < HEIGHT:  # DOWN
+        red.y += VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.height < WIDTH:  # RIGHT
+        red.x += VEL
+
+
+def handle_bullets(yellow_bullets, red_bullets, yellow, red):
+    for bullet in yellow_bullets:
+        bullet.x += BULLET_VEL
+        if red.colliderect(bullet): # hit red
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            yellow_bullets.remove(bullet)
+        if bullet.x > WIDTH:
+            yellow_bullets.remove(bullet)
+
+    for bullet in red_bullets:
+        bullet.x -= BULLET_VEL
+        if yellow.colliderect(bullet): # hit yellow
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            red_bullets.remove(bullet)
+        if bullet.x < 0:
+            red_bullets.remove(bullet)
+
+
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text, 1, WHITE)
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width()/2, HEIGHT/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(5000)
+
+
 def main():
+    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+    red_bullets = []
+    yellow_bullets = []
+
+    red_health = 10
+    yellow_health = 10
+
     clock = pygame.time.Clock()
     run = True
     # game loop
@@ -26,9 +132,43 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # press X on the top of the pygame window
                 run = False
-        draw_window()
+                pygame.quit()
 
-    pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        yellow.x + yellow.height, yellow.y + yellow.width/2 - 2, 10, 5)
+                    yellow_bullets.append(bullet)
+
+                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
+                    bullet = pygame.Rect(
+                        red.x, red.y + red.width/2 - 2, 10, 5)
+                    red_bullets.append(bullet)
+                
+            if event.type == RED_HIT:
+                red_health -= 1
+            if event.type == YELLOW_HIT:
+                yellow_health -= 1
+
+        
+        winner_text = ""
+        if red_health <= 0:
+            winner_text = "Yellow WINs!"
+        if yellow_health <= 0:
+            winner_text = "Red WINs!"
+        if winner_text != "":
+            draw_winner(winner_text)
+            break
+        
+
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+
+        handle_bullets(yellow_bullets, red_bullets, yellow, red)
+        draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
+
+    main()
 
 
 if __name__ == "__main__":
